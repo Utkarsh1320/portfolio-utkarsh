9b0d11b9eb3d9157174f0c75e5d812a0d88da42a
Utkarsh More <utkarsh.more.2000@gmail.com>
2024-12-07 14:56:11 -0500

    Font Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
     return (
         <>
             <div className="flex justify-between p-10">
-                <div className="w-1/4 font-bold items-center justify-right text-center">Logo</div>
+                <div className="w-1/4 font-Embed items-center justify-right text-center">Logo</div>
                 <div className="w-2/4 ">
                     <ul className="flex items-center justify space-x-10 h-full">
-                        <li className="flex-1 text-center font-mono">About</li>
-                        <li className="flex-1 text-center font-mono">Careers</li>
-                        <li className="flex-1 text-center font-mono">Contact</li>
+                        <li className="flex-1 text-center font-Embed">About</li>
+                        <li className="flex-1 text-center font-Embed">Careers</li>
+                        <li className="flex-1 text-center font-Embed">Contact</li>
                     </ul>
 
                 </div>
