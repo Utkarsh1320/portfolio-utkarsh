564cf1b94f81ce04a581de51e6426aef4bb5170b
Utkarsh More <utkarsh.more.2000@gmail.com>
2024-12-07 15:07:42 -0500

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,17 @@
 const Navbar = () => {
     return (
         <>
-            <div className="flex justify-between p-10">
-                <div className="w-1/4 font-Embed items-center justify-right text-center">Logo</div>
+            <div className="flex justify-between p-10 w-3/4 mx-auto">
+                <div className=" font-bold items-center justify-right text-center text-xl">Logo</div>
                 <div className="w-2/4 ">
-                    <ul className="flex items-center justify space-x-10 h-full">
-                        <li className="flex-1 text-center font-Embed">About</li>
-                        <li className="flex-1 text-center font-Embed">Careers</li>
-                        <li className="flex-1 text-center font-Embed">Contact</li>
+                    <ul className="flex space-x-10 h-full">
+                        <li className="flex-1 text-right font-Embed text-xl">About</li>
+                        <li className="flex-1 text-right font-Embed text-xl">Careers</li>
+                        <li className="flex-1 text-right font-Embed text-xl">Contact</li>
                     </ul>
-
                 </div>
-
             </div>
+            <div className="border-b border-gray-400 w-3/4 mx-auto"></div>
         </>
     )
 }
