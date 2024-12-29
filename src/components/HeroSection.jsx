0317e3518643753833d082a8d9cadb11ffa59ffc
Utkarsh More <utkarsh.more.2000@gmail.com>
2024-12-29 15:49:45 -0500

    Hero Desktop completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,6 @@
 import headshot from '../assets/headShot.jpg'
+import github from '../assets/github_svg.svg'
+import linkedin from '../assets/LinkedIn_svg.svg'
 import Navbar from "../components/Navbar"
 const HeroSection = () => {
     return (
@@ -11,16 +13,16 @@ const HeroSection = () => {
                     </div>
 
                     <section className='justify-items-center content-center'>
-                        <h3 className='text-lg'>Hello I'm</h3>
-                        <h1>Utkarsh More</h1>
-                        <h2>Frontend Developer</h2>
-                        <div>
-                            <button>Download CV</button>
-                            <button>Contacnt Info</button>
+                        <h3 className='text-2xl'>Hello I'm</h3>
+                        <h1 className='text-6xl font-bold'>Utkarsh More</h1>
+                        <h2 className='text-4xl text-gray-400 font-semibold py-3'>Frontend Developer</h2>
+                        <div className='pt-12 grid grid-cols-2 gap-4'>
+                            <button className='py-3 px-6 border-2 border-black rounded-full '>Download CV</button>
+                            <button className='py-3 px-6 border-2 rounded-full text-white bg-black '>Contact Info</button>
                         </div>
-                        <div>
-                            <img src="" />
-                            <img src="" />
+                        <div className='grid grid-cols-2 gap-3 mt-6'>
+                            <img src={github} />
+                            <img src={linkedin} />
                         </div>
                     </section>
                 </div>
