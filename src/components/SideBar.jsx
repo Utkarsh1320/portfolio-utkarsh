962c4ed7d8cfb1cee1af05614ad6d145814819d1
Utkarsh <utkarsh.more.2000@gmail.com>
2025-02-01 19:52:17 -0500

    mobile improvements

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -39,47 +39,49 @@ const sidebarBottomItems = [
 function SideBar() {
   const location = useLocation();
   return (
-    <aside className="flex w-[4.2vw] min-w-[40px] flex-col justify-between bg-sidebarBg  py-2 md:min-h-[calc(100vh-65px)] border border-blue-950">
-      <div className="flex flex-col items-start  justify-center gap-y-1 md:gap-y-3 ">
-        {sidebarTopItems.map(({ Icon, path }) => {
-          return (
-            <Link to={`${path}`} key={path}>
-              <div
-                className={`${location.pathname === path
-                  ? "border-l-2 border-accentColor"
-                  : ""
-                  }  p-2  max-sm:pl-2`}
-              >
-                <Icon
-                  fill={
-                    location.pathname === path
-                      ? "rgb(225, 228, 232)"
-                      : "rgb(106, 115, 125)"
-                  }
-                />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-      <div className="flex flex-col items-center justify-center gap-y-3  ">
-        {sidebarBottomItems.map(({ Icon, path }) => {
-          return (
-            <Link to={`${path}`} key={path}>
-              <div className="p-2  max-sm:pl-2">
-                <Icon
-                  fill={
-                    location.pathname === path
-                      ? "rgb(225, 228, 232)"
-                      : "rgb(106, 115, 125)"
-                  }
-                />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </aside>
+    <div className="sm: hidden md:block">
+      <aside className="flex w-[4.2vw] min-w-[40px] flex-col justify-between bg-sidebarBg  py-2 md:min-h-[calc(100vh-65px)] border border-blue-950">
+        <div className="flex flex-col items-start  justify-center gap-y-1 md:gap-y-3 ">
+          {sidebarTopItems.map(({ Icon, path }) => {
+            return (
+              <Link to={`${path}`} key={path}>
+                <div
+                  className={`${location.pathname === path
+                    ? "border-l-2 border-accentColor"
+                    : ""
+                    }  p-2  max-sm:pl-2`}
+                >
+                  <Icon
+                    fill={
+                      location.pathname === path
+                        ? "rgb(225, 228, 232)"
+                        : "rgb(106, 115, 125)"
+                    }
+                  />
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex flex-col items-center justify-center gap-y-3  ">
+          {sidebarBottomItems.map(({ Icon, path }) => {
+            return (
+              <Link to={`${path}`} key={path}>
+                <div className="p-2  max-sm:pl-2">
+                  <Icon
+                    fill={
+                      location.pathname === path
+                        ? "rgb(225, 228, 232)"
+                        : "rgb(106, 115, 125)"
+                    }
+                  />
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </aside>
+    </div>
   );
 }
 // h-[calc(100vh-38.5px)]
